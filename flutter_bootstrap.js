2c8d22ccc71edec3ef473903710898a0d5942bbe
meentrpni <meentrps@gmail.com>
2025-12-24 16:24:24 +0700

    update web

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"18818009497c581ede5d8a3b8b833b81d00ceb
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4008926264"
+    serviceWorkerVersion: "765162626"
   }
 });
